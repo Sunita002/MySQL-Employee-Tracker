9121b1bf560702f6d982f228c1cfc6a9acf2c9c8
sunita persad <sunitapersad020@gmail.com>
2021-11-14 20:07:55 -0500

    selections for row 4 on employee table

--- assets/seed.sql
+++ assets/seed.sql
@@ -30,4 +30,4 @@ SELECT employee.id, employee.first_name, employee.last_name, role.title, departm
   INNER JOIN department ON (department.id = role.department_id)
   ORDER BY employee.id;
 
-  
+  SELECT first_name, last_name, role_id FROM employee 	WHERE employee.id = 4;
