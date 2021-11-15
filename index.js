3c5adaf0bd48f1fa04192cb1410067109108af1d
sunita persad <sunitapersad020@gmail.com>
2021-11-14 19:34:01 -0500

    messages for prompts

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const mysql = require('mysql');
 const mysql2 = require('mysql2');
 const inquirer = require('inquirer');
 require('console.table');
+
+const promptMessages = {
+    viewAllEmployees: "View All Employees",
+    viewByDepartment: "View All Employees By Department",
+    viewByManager: "View All Employees By Manager",
+    addEmployee: "Add An Employee",
+    removeEmployee: "Remove An Employee",
+    updateRole: "Update Employee Role",
+    updateEmployeeManager: "Update Employee Manager",
+    viewAllRoles: "View All Roles",
+    exit: "Exit"
+};
