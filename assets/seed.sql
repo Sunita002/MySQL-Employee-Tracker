dbbcdf4d1f17ac46bf468970001b357dfe85c4fc
sunita persad <sunitapersad020@gmail.com>
2021-11-14 20:07:05 -0500

    select employee, role, dept, salary from employee

--- assets/seed.sql
+++ assets/seed.sql
@@ -22,3 +22,12 @@ SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department
     LEFT JOIN role ON (role.id = employee.role_id)
     LEFT JOIN department ON (department.id = role.department_id)
     ORDER BY role.title;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN employee manager on manager.id = employee.manager_id
+  INNER JOIN role ON (role.id = employee.role_id)
+  INNER JOIN department ON (department.id = role.department_id)
+  ORDER BY employee.id;
+
+  
