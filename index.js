3353977874fcc163aed39842ff676e48abf3c061
sunita persad <sunitapersad020@gmail.com>
2021-11-14 19:36:21 -0500

    mysql connection function

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ const promptMessages = {
     viewAllRoles: "View All Roles",
     exit: "Exit"
 };
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: 'root',
+
+    // Your password
+    password: 'MyNewPass',
+    database: 'employees'
+});
